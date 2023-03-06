08c76f22c3504cf0af24dc469cae138bbade8f0f
viniciusmm10 <viniciusmm10@outlook.com>
2023-03-06 17:40:04 -0300

    mod06.03

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon')
-const nav = document.querySelector('nav')
-
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('menu-open')
-})
