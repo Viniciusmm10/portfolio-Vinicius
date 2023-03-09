6dbed08f98906427f9f59ef698f4ac0e91957126
viniciusmm10 <viniciusmm10@outlook.com>
2023-03-09 15:19:33 -0300

    modform

---
+++ form/form.js
@@ -0,0 +1,16 @@
+let containers = document.querySelectorAll('.input-box')
+let inputs = document.querySelectorAll('.senha')
+let icons = document.querySelectorAll('.toggle-icon')
+
+for (let i = 0; i < icons.length; i++) {
+  icons[i].addEventListener('click', function () {
+    containers[i].classList.toggle('visible')
+    if (containers[i].classList.contains('visible')) {
+      icons[i].src = 'assets/eye.svg'
+      inputs[i].type = 'text'
+    } else {
+      icons[i].src = 'assets/eye-off.svg'
+      inputs[i].type = 'password'
+    }
+  })
+}
